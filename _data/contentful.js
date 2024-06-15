868e0cf10508ced4173e4fdd48a05f28b96de472
DannyBoyNYC <daniel.deverell@gmail.com>
2024-06-15 15:45:55 -0400

    adding image plugin

--- _data/contentful.js
+++ _data/contentful.js
@@ -10,7 +10,7 @@ const client = contentful.createClient({
 
 module.exports = function () {
   return client
-    .getEntries({ order: "sys.updatedAt" })
+    .getEntries({ content_type: "post", order: "sys.updatedAt" })
     .then((response) => {
       const page = response.items.map((page) => {
         page.fields.date = new Date(page.sys.updatedAt);
@@ -23,3 +23,20 @@ module.exports = function () {
     })
     .catch(console.error);
 };
+
+// module.exports = function () {
+//   return client
+//     .getEntries({ content_type: "page", order: "sys.updatedAt" })
+//     .then((response) => {
+//       const about = response.items.map((about) => {
+//         about.fields.date = new Date(about.sys.updatedAt);
+//         about.fields.body =
+//           about.fields.body && marked.parse(about.fields.body);
+//         // some images are undefined
+//         about.fields.imageUrl = about.fields.heroImage?.fields?.file?.url;
+//         return about.fields;
+//       });
+//       return about;
+//     })
+//     .catch(console.error);
+// };
