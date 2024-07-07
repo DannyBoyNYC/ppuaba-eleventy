f4dba2f50d588f428dc092d32eec19574890d2b6
DannyBoyNYC <daniel.deverell@gmail.com>
2024-07-07 12:40:45 -0400

    formatting pages

--- src/_includes/components/Page.js
+++ src/_includes/components/Page.js
@@ -8,15 +8,22 @@ let options = {
 };
 
 Page = (title, date, imageUrl, body) => `
-      ${imageProcessing(imageUrl, title)}
-      <h2 class="page__header>${title}</h2>
+  <section class="page__header">
+    <div class="page__header__image">
+<div style="max-width: 1800px; dispay: block">
+      <img src="${imageUrl}" alt="${title}" />
+      </div>
+    </div>
+      <h1>${title}</h1>
       <h3 class="date">Updated: ${new Intl.DateTimeFormat(
         "en-US",
         options
       ).format(date)}</h3>
-      
+  </section>
+  <section>
       <div className="body">
       ${body}
-      </div>`;
+      </div>
+  </section>`;
 
 module.exports = Page;
