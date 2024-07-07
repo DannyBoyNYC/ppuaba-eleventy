05c68feef6783f1104c043bf41433c28b1e7fb45
DannyBoyNYC <daniel.deverell@gmail.com>
2024-07-07 11:42:12 -0400

    palced files into src directory

--- .eleventy.js
+++ .eleventy.js
@@ -8,8 +8,8 @@ const client = contentful.createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_DELIVERY,
 });
 
-const Card = require("./_includes/components/Card");
-const Page = require("./_includes/components/Page");
+const Card = require("./src/_includes/components/Card");
+const Page = require("./src/_includes/components/Page");
 
 // const asset = client
 //   .getAsset("8Ou4Gl5U3cDO0T4BLYCEQ")
@@ -20,112 +20,18 @@ const Page = require("./_includes/components/Page");
 // } = require('@contentful/rich-text-html-renderer');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
-  // eleventyConfig.addPassthroughCopy("images");
+  // eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
   // eleventyConfig.addShortcode('documentToHtmlString', documentToHtmlString);
   // eleventyConfig.addShortcode("imageProcessing", imageProcessing);
   eleventyConfig.addShortcode("marked", marked);
-
   eleventyConfig.addShortcode("Card", Card);
   eleventyConfig.addShortcode("Page", Page);
-  // eleventyConfig.addShortcode("page", function (page) {
-  //   return `
-  //     <section id="story">
-  //     <h1>
-  //       {{ page.title }}
-  //     </h1>
-  //     <img src="{{ page.imageUrl }}" alt="{{ page.title }}" />
-  //     <div class="inner">
-  //       <time>{{ page.date | date : "%Y-%m-%d" }}</time>
-  //       {{ page.body }}
-  //     </div>
-  //     </section>`;
-  // });
-
-  // eleventyConfig.addShortcode("bannerBlock", function(bannerBlock) {
-  //     return `
-  //                 <section id="wrapper">
-  //                     <header id="${bannerBlock.fields.sectionLink}">
-  //                         <div class="inner">
-  //                             <h2>${bannerBlock.fields.sectionTitle}</h2>
-  //                             ${ documentToHtmlString(bannerBlock.fields.content) }
-  //                         </div>
-  //                     </header>
-  //                 </section>`;
-  // });
-  // eleventyConfig.addShortcode("contentBlock", function(contentBlock) {
-  //     return `
-  //                 <section id="${contentBlock.fields.sectionLink}">
-  //                     <div class="wrapper">
-  //                         <div class="inner">
-  //                             <h3 class="major">${contentBlock.fields.sectionTitle}</h3>
-  //                             ${ documentToHtmlString(contentBlock.fields.content) }
-  //                         </div>
-  //                     </div>
-  //                 </section>`;
-  // });
-
-  // eleventyConfig.addShortcode("footerBlock", function(footerBlock) {
-  //     return `
-  //                 <section id="footer">
-  //                     <div class="inner">
-  //                         <div class="copyright">
-  //                             ${ marked(footerBlock.fields.content) }
-  //                         </div>
-  //                     </div>
-  //                 </section>`;
-  // });
-
-  // eleventyConfig.addShortcode("cardBlock", async function(cardBlock) {
-  //     const output = await Promise.all(cardBlock.fields.cards
-  //         .map(({
-  //             sys
-  //         }) => {
-  //             return cards = client.getEntry(sys.id)
-  //                 .then((card) => {
-  //                     return `<article>
-  //                                 <a href="#" class="image">${imageProcessing(card.fields.image)}</a>
-  //                                 <h3 class="major">${card.fields.sectionTitle}</h3>
-  //                                 ${ documentToHtmlString(card.fields.content) }
-  //                             </article>`;
-  //                 })
-  //         }));
-  //     return `
-  //                 <section id="${cardBlock.fields.sectionLink}" class="wrapper alt style1">
-  //                     <div class="inner">
-  //                         <h2 class="major">${cardBlock.fields.sectionTitle}</h2>
-  //                         <section class="features">
-  //                             ${ output.join('') }
-  //                         </section>
-  //                     </div>
-  //                 </section>`;
-  // });
-
-  // eleventyConfig.addShortcode("featuretteBlock", function(
-  //     featuretteBlock) {
-  //     if(featuretteBlock.fields.imageLocation) {
-  //         return `
-  //                     <section id="${featuretteBlock.fields.sectionLink}" class="wrapper spotlight style1">
-  //                         <div class="inner">
-  //                             <a href="#" class="image">${imageProcessing(featuretteBlock.fields.image)}</a>
-  //                             <div class="content">
-  //                                 <h2 class="major">${featuretteBlock.fields.sectionTitle}</h2>
-  //                                 ${ documentToHtmlString(featuretteBlock.fields.content) }
-  //                             </div>
-  //                         </div>
-  //                     </section>`;
-  //     }
-  //     else {
-  //         return `
-  //                     <section id="${featuretteBlock.fields.sectionLink}" class="wrapper alt spotlight style2">
-  //                         <div class="inner">
-  //                             <a href="#" class="image">${imageProcessing(featuretteBlock.fields.image)}</a>
-  //                             <div class="content">
-  //                                 <h2 class="major">${featuretteBlock.fields.sectionTitle}</h2>
-  //                                 ${ documentToHtmlString(featuretteBlock.fields.content) }
-  //                             </div>
-  //                         </div>
-  //                     </section>`;
-  //     }
-  // });
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
 };
